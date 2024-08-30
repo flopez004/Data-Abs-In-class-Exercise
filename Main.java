5f8f7b2cec25f269f01f5adb7b50c286fc9d740e
flopez004 <159561504+flopez004@users.noreply.github.com>
2024-08-30 16:55:15 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    int[] numArray= new int[values];
+    Arrays.Sort(numArray);
+    for(int i = 0; i< numArray.length -1; i++){
+        if(numArray[i]== numArray[i+1]){
+            return false;
+        }
+
+    }
+    return true;
+
+
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} 

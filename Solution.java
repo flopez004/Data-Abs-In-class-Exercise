f2605d6c770c35d4899d9ae56341b30fa34235dd
flopez004 <159561504+flopez004@users.noreply.github.com>
2024-09-06 22:58:05 -0500

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+public class Solution {
+    public boolean isValid(String s){
+        if(s.length() <= 1) {
+            return false; 
+        }
+    
+        Stack<Character> stank = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+                stank.push(s.charAt(i));
+            }
+            else{
+                if(s.charAt(i) == ')') {
+                    if(stank.pop() != '(') {
+                        return false;
+                    }
+                }
+                if(s.charAt(i) == '}') {
+                    if(stank.pop() != '{') {
+                        return false;
+                    }
+                }
+            }
+        }
+        if (stank.isEmpty()) {
+            System.out.println("Stack is empty");
+        }
+        else {
+            System.out.println("Stack is not empty");
+        }
+    }
+}
+
